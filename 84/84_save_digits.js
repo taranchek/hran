527c25b3dd104d201d2ac8d1816fc278832593a9
taranchek <117262880+taranchek@users.noreply.github.com>
2023-06-29 11:27:25 +0300

    Add files via upload

---
+++ 84/84_save_digits.js
@@ -0,0 +1,3 @@
+localStorage.setItem('first_digit', "1")
+localStorage.setItem('second_digit', "2")
+localStorage.setItem('third_digit', "3")
---
+++ 84/84_sum_digits.js
@@ -0,0 +1,7 @@
+let first = Number(localStorage.getItem('first_digit'))
+let second = Number(localStorage.getItem('second_digit'))
+let third = Number(localStorage.getItem('third_digit'))
+
+let sum = first + second + third;
+
+console.log(sum)
